ee953c6b0ccc087c86f3cdcb3838bf328ad66f3d
Thiago junior <thi4go19@gmail.com>
2024-02-21 11:26:29 -0300

    inicial

---
+++ 1_intro/info.php
@@ -0,0 +1,11 @@
+<?php
+
+  phpinfo();
+  
+  /*
+phpinfo() = Mostra todas as configurações relacionadas com o PHP (Dependências também)
+Mostrar as configurações relacionadas ao PHP:
+logs, configurações, portas, limite upload, upload is ativo, time zone, variáveis de ambiente.
+*/
+
+?>
